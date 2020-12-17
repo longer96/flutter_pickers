23839d6dee1cae7410a1aee0afacd6e08218f769
longer <wujunlong2011@qq.com>
2020-12-17 11:05:43 +0800

    first commit

---
+++ test/flutter_picker_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_pickers/address_picker/data.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('adds one to input values', () {
+    Address.getCityNameByCode(provinceCode: "510000", cityCode: "510100", townCode: "510104");
+    Address.getCityNameByCode(provinceCode: "510000", cityCode: "510100", townCode: "5104");
+    Address.getCityNameByCode(provinceCode: "510000", cityCode: "510102");
+    Address.getCityNameByCode(provinceCode: "510000", cityCode: "510100");
+    Address.getCityNameByCode(provinceCode: "510000", cityCode: "5101020");
+    Address.getCityNameByCode(provinceCode: "5100", cityCode: "5101020");
+    Address.getCityNameByCode();
+  });
+}
